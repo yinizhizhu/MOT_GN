5190ed300d9acd8dda7b17d458a11e26b6a3b5e4
yinizhizhu <lijiahe19941013@sina.cn>
2018-10-03 08:42:09 +0800

    Sample collection & Fine-Tuning

--- torch_0.4/Finetune/finetune.py
+++ torch_0.4/Finetune/finetune.py
@@ -1,14 +1,11 @@
-from torchvision.transforms import ToTensor
-import numpy as np
+import time
 import torch.nn as nn
 import torch.optim as optim
-import torch, cv2, torchvision, os
-from PIL import Image
+import torch, torchvision
+from torch.utils.data import DataLoader
+from finetune_dataset import DatasetFromFolder
 
-
-def load_img(filepath):
-    img = Image.open(filepath).convert('RGB')
-    return img
+from tensorboardX import SummaryWriter
 
 
 class appearance(nn.Module):
@@ -25,139 +22,86 @@ class appearance(nn.Module):
 class finetuning():
     def __init__(self, cuda=False):
         self.device = torch.device('cuda' if cuda else 'cpu')
+        self.nEpochs = 10
+        self.numWorker = 4
+        self.batchsize = 8
         self.loadModel()
-        self.trainDir = 'MOT16/train/'
-        self.optimizer = optim.Adam(self.Appearance.parameters(), lr=1e-3)
+        self.optimizer = optim.Adam(self.Appearance.parameters(), lr=5e-4)
+        self.train_set = DatasetFromFolder()
+        self.data_loader = DataLoader(dataset=self.train_set, num_workers=self.numWorker, batch_size=self.batchsize,
+                                 shuffle=True)
 
     def loadModel(self):
         self.Appearance = appearance()
         self.Appearance.to(self.device)
-        self.Appearance.train()  # Essential step - to avoid the effect of BatchNormalization
+        self.Appearance.train()  # Training model
 
-    def reset(self):
-        self.images = [[] for i in xrange(4)]  # the appearance of bbxes
-
-    def loadData(self, show=0):
-        """
-        :param tag: 0 - train, 1 - test
-        :param show: 1 - show the image and crop in real-time
-        :return: None
-        """
-        self.reset()
-        trainList = ['in_place']
-        for part in trainList:
-            part = self.trainDir+part
-
-            # get the length of the sequence
-            info = part+'/seqinfo.ini'
-            f = open(info, 'r')
-            f.readline()
-            for line in f.readlines():
-                line = line.strip().split('=')
-                if line[0] == 'seqLength':
-                    seqL = int(line[1])
-            f.close()
-            seqL = 10
-
-            # read the image
-            imgs = [0] # store the sequence
-            imgDir = part+'/img1/'
-            for i in xrange(1, seqL+1):
-                img = load_img(imgDir + '%06d.jpg'%i)
-                imgs.append(img)
-
-            gt = part + '/gt/gt.txt'
-            f = open(gt, 'r')
-            if show:
-                cv2.namedWindow('view', flags=0)
-                cv2.namedWindow('crop', flags=0)
-            for line in f.readlines():
-                line = line.strip().split(',')
-                index = int(line[0])
-                if index > seqL:
-                    continue
-                id = int(line[1]) - 1
-                x, y = int(float(line[2])), int(float(line[3]))
-                w, h = int(float(line[4])), int(float(line[5]))
-
-                img = imgs[index]
-                crop = imgs[index].crop([x, y, x+w, y+h])
-                bbx = crop.resize((224, 224), Image.ANTIALIAS)
-                self.images[id].append(bbx)
-
-                if show:
-                    img = np.asarray(img)
-                    crop = np.asarray(crop)
-                    print line
-                    print part, index, seqL, '%06d'%index
-                    print w, h
-                    print len(crop[0]), len(crop)
-                    cv2.imshow('crop', crop)
-                    cv2.imshow('view', img)
-                    cv2.waitKey(34)
-            if show:
-                raw_input('Continue?')
-            f.close()
-
-    def resnet34(self, img):
-        bbx = ToTensor()(img)
-        bbx = bbx.to(self.device)
-        bbx = bbx.view(-1, bbx.size(0), bbx.size(1), bbx.size(2))
-        return bbx
+        f = open('Fine-tune/finetune.txt', 'w')
+        print >> f, self.Appearance
+        f.close()
 
     def mse(self, a, b):
         c = a - b
         ret = torch.mean(c*c)
         return ret
 
+    def saveModel(self, epoch):
+        self.writer.export_scalars_to_json('Fine-tune/scalars_%02d.json'%epoch)
+        self.writer.close()
+
+        print 'Saving the Appearance model...'
+        self.Appearance.eval()
+        torch.save(self.Appearance, 'Fine-tune/appearance_%02d.pth'%epoch)
+        self.Appearance.train()
+
     def finetune(self):
         step = 0
-        f = open('finetune.txt', 'w')
-        for anchor in xrange(4):
-            anchor_n = len(self.images[anchor])
-            for anchor_i in xrange(anchor_n):
-                img = self.images[anchor][anchor_i]  # the anchor image
-                img_v = self.resnet34(img)
-                for anchor_j in xrange(anchor_n):
-                    if anchor_j != anchor_i:
-                        ps = self.images[anchor][anchor_j]  # the positive sample
-                        ps_v = self.resnet34(ps)
-                        for neg in xrange(4):
-                            if anchor != neg:
-                                neg_n = len(self.images[neg])
-                                for neg_i in xrange(neg_n):
-                                    one = torch.FloatTensor([1.0]).to(self.device)
-                                    zero = torch.FloatTensor([0.0]).to(self.device)
-                                    ns = self.images[neg][neg_i]  # the negative sample
-                                    ns_v = self.resnet34(ns)
-                                    ns_f = self.Appearance(ns_v)    # negative
-                                    img_f = self.Appearance(img_v)  # anchor
-                                    ps_f = self.Appearance(ps_v)    # positive
+        head = time.time()
+        for epoch in xrange(1, self.nEpochs):
+            self.writer = SummaryWriter()
+            start = time.time()
+            num = 0
+            epoch_loss = 0.0
+            for iteration in enumerate(self.data_loader, 1):
+                index, (anchor, positive, negative) = iteration
 
-                                    self.optimizer.zero_grad()
-                                    loss1 = self.mse(img_f, ns_f)/2
-                                    epoch_loss = loss1.item()
-                                    print >> f, step, epoch_loss,
+                a = self.Appearance(anchor.to(self.device))
+                p = self.Appearance(positive.to(self.device))
+                n = self.Appearance(negative.to(self.device))
 
-                                    loss2 = self.mse(ps_f, ns_f)/2
-                                    epoch_loss = loss2.item()
-                                    print >> f, epoch_loss,
+                # Penalize the u to let its value not too big
 
-                                    loss3 = self.mse(img_f, ps_f)
-                                    epoch_loss = loss3.item()
-                                    print >> f, epoch_loss
-                                    triplet_loss = one+loss3-loss1-loss2
-                                    loss = torch.max(zero, triplet_loss)
-                                    loss.backward()
-                                    self.optimizer.step()
-                                    step += 1
-        f.close()
-        print 'Finish the finetuning!'
+                one = torch.FloatTensor([1.0]).to(self.device)
+                zero = torch.FloatTensor([0.0]).to(self.device)
+
+                self.optimizer.zero_grad()
+                d_a_p = self.mse(a, p)
+                loss1 = d_a_p.item()
+
+                d_a_n = self.mse(a, n)
+                loss2 = d_a_n.item()
+
+                triplet_loss = one + d_a_p - d_a_n
+                loss = torch.max(zero, triplet_loss)
+                epoch_loss += loss.item()
+                loss.backward()
+                self.optimizer.step()
+
+                self.writer.add_scalars('Step_loss', {'Loss1': loss1, 'Loss2': loss2}, step)
+
+                num += self.batchsize
+                step += 1
+                break
+
+            print ' Time consuming:', time.time()-start
+            epoch_loss /= num
+            self.writer.add_scalars('Epoch_loss', {'Loss': epoch_loss}, epoch)
+            self.saveModel(epoch)
+
+        print 'Time consuming:', time.time() - head
 try:
     ft = finetuning()
-    print 'Loading data...'
-    ft.loadData()
-    print 'Finetuning...'
+    print '     Finetuning...'
     ft.finetune()
 except KeyboardInterrupt:
     print ''
---
+++ torch_0.4/Finetune/finetune_dataset.py
@@ -0,0 +1,221 @@
+import random, time, cv2, torch
+from torchvision.transforms import ToTensor
+import numpy as np
+from math import *
+from PIL import Image
+import torch.utils.data as data
+from global_set import tau_vr, tau_frame
+
+
+def load_img(filepath):
+    img = Image.open(filepath).convert('RGB')
+    return img
+
+p_tag = 0  # 1 - nearby, 0 - no limitation
+
+
+class DatasetFromFolder(data.Dataset):
+    def __init__(self):
+        super(DatasetFromFolder, self).__init__()
+        basic_dir = 'MOT16/train/MOT16-'
+        parts = ['02', '04', '05', '09', '10', '11', '13']
+        self.candidates = []
+        self.counter = 0
+
+        start = time.time()
+        for part in parts:
+            head = time.time()
+            print '     ', part,
+            part = basic_dir+part
+            self.img_dir = part + '/img1/'
+            self.gt_dir = part + '/gt/'
+            self.det_dir = part + '/det/'
+
+            self.getSeqL(part)
+            self.readImg()
+            self.readBBx()
+            self.sampleCollection()
+            print '     Time consuming:', (time.time()-head)/60.0
+            # break
+        print 'Time consuming:', (time.time()-start)/60.0
+
+    def getSeqL(self, part):
+        # get the length of the sequence
+        info = part+'/seqinfo.ini'
+        f = open(info, 'r')
+        f.readline()
+        for line in f.readlines():
+            line = line.strip().split('=')
+            if line[0] == 'seqLength':
+                self.seqL = int(line[1])
+        f.close()
+        print ' *  The length of the sequence:', self.seqL, ' * ',
+
+        # get the number of the tracks
+        gt = self.gt_dir + 'gt.txt'
+        f = open(gt, 'r')
+        self.track_num = 0
+        for line in f.readlines():
+            line = line.strip().split(',')
+            id = int(line[1])
+            if id > self.track_num:
+                self.track_num = id
+        f.close()
+        print 'The number of the tracks:', self.track_num
+
+    def readBBx(self):
+        # get the gt
+        self.bbx = [[] for i in xrange(self.track_num+1)]
+        self.look_up = [[] for i in xrange(self.seqL + 1)]
+        gt = self.gt_dir + 'gt.txt'
+        f = open(gt, 'r')
+        pre = -1
+        for line in f.readlines():
+            line = line.strip().split(',')
+            if line[7] == '1':
+                index = int(line[0])
+                id = int(line[1])
+                x, y = int(line[2]), int(line[3])
+                w, h = int(line[4]), int(line[5])
+                conf_score, l, vr = float(line[6]), int(line[7]), float(line[8])
+
+                # sweep the invisible head-bbx from the training data
+                if pre != id and vr == 0:
+                    continue
+
+                pre = id
+                # x, y, w, h, frame_number
+                if vr >= tau_vr:
+                    self.bbx[id].append([x, y, w, h, index])
+                    self.look_up[index].append([x, y, w, h])
+        f.close()
+
+    def distance(self, a_bbx, b_bbx):
+        delta_x = a_bbx[0]-b_bbx[0]
+        delta_y = a_bbx[1]-b_bbx[1]
+        return sqrt(delta_x*delta_x + delta_y*delta_y)
+
+    def positive(self, id, i):
+        bbx = self.bbx[id][i]
+        x, y, w, h, index = bbx
+        n = len(self.bbx[id])
+        if p_tag:
+            head, tail = i, i
+
+            step = head-1
+            while step > 0 and abs(index - self.bbx[id][step][4]) < tau_frame:
+                head = step
+
+            step = tail+1
+            while step < n and abs(index-self.bbx[id][step][4]) < tau_frame:
+                tail = step
+        else:
+            head, tail = 0, n-1
+
+        while tail - head:
+            step = random.randint(head, tail)
+            if step != i:
+                return self.bbx[id][step]
+        return None
+
+    def negative(self, bbx, index):
+        n = len(self.look_up[index])
+        if n < 2:
+            return None
+        container = []
+        for i in xrange(n):
+            container.append([i, self.distance(bbx, self.look_up[index][i])])
+        container = sorted(container, key=lambda a: a[1])
+        i = random.randint(1, min(n-1, 4))
+        # print i, n
+        try:
+            i = container[i][0]
+        except IndexError:
+            print i, n
+            print container
+        return self.look_up[index][i]
+
+    def readImg(self):
+        self.imgs = [None]
+        for i in xrange(1, self.seqL+1):
+            img = load_img(self.img_dir+'%06d.jpg'%i)
+            self.imgs.append(img)
+
+    def fixBB(self, x, y, w, h, size):
+        width, height = size
+        w = min(w+x, width)
+        h = min(h+y, height)
+        x = max(x, 0)
+        y = max(y, 0)
+        w -= x
+        h -= y
+        return x, y, w, h
+
+    def cropPatch(self, bbx, index=None):
+        if index is None:
+            x, y, w, h, index = bbx
+        else:
+            x, y, w, h = bbx
+        img = self.imgs[index]
+        x, y, w, h = self.fixBB(x, y, w, h, img.size)
+        crop = img.crop([x, y, x + w, y + h])
+        patch = crop.resize((224, 224), Image.ANTIALIAS)
+        bbx = ToTensor()(patch)
+        # bbx = bbx.view(-1, bbx.size(0), bbx.size(1), bbx.size(2))
+        return bbx
+
+    def sampleCollection(self, show=0):
+        if show:
+            cv2.namedWindow('anchor', flags=0)
+            cv2.namedWindow('positive', flags=0)
+            cv2.namedWindow('negative', flags=0)
+        f = open('Fine-tune/finetune.txt', 'a')
+        print >> f, self.gt_dir
+        counter = 0
+        container = []
+        for i in xrange(1, self.track_num+1):
+            num = len(self.bbx[i])
+            if num > 1:
+                container.append(i)
+                print >> f, i, num
+                counter += 1
+        self.counter += counter
+        print >> f, 'Number:', counter, self.counter
+        f.close()
+
+        for i in xrange(counter):
+            step = 0
+            while step < 30:
+                index = container[i]
+                positive, negative = None, None
+                while positive is None or negative is None:
+                    a_index = random.randint(0, len(self.bbx[index])-1)
+                    anchor = self.bbx[index][a_index]
+                    positive = self.positive(index, a_index)
+                    negative = self.negative(anchor, index)
+
+                a = self.cropPatch(anchor)
+                p = self.cropPatch(positive)
+                n = self.cropPatch(negative, index)
+                if show:
+                    img = np.asarray(a)
+                    cv2.imshow('anchor', img)
+                    crop1 = np.asarray(p)
+                    cv2.imshow('positive', crop1)
+                    crop2 = np.asarray(n)
+                    cv2.imshow('negative', crop2)
+                    cv2.waitKey(300)
+                    # raw_input('Continue?')
+                self.candidates.append([a, p, n])
+                step += 1
+
+        # free up the space
+        self.imgs = []
+        self.bbx = []
+        self.look_up = []
+
+    def __getitem__(self, index):
+        return self.candidates[index]
+
+    def __len__(self):
+        return len(self.candidates)
